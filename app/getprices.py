35a1f864c3e306ef52c5bd72fa634cc1a89058f9
Kyle Good <kwgood@gmail.com>
2022-06-12 17:16:58 -0400

    Taking out API key

--- app/getprices.py
+++ app/getprices.py
@@ -1,4 +1,4 @@
 import requests
 
-response = requests.get("https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2020-06-01/2020-06-17?apiKey=bXSJUxjypggininnkMAGgNsf2utFs2YU")
+response = requests.get("https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2020-06-01/2020-06-17?apiKey=")
 print(response)
